72ffb57407c730fe6a6b979aa71181d832d5de0e
changbaowang <Ace64532866@outlook.com>
2017-03-15 20:18:12 +0800

    Binary Search

---
+++ app/src/main/java/ace/know/you/ado/binarysearch/MainActivity.java
@@ -0,0 +1,44 @@
+package ace.know.you.ado.binarysearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Integer [] arrTest = new Integer[]{0,1,2,3,4,5,6,7,8,9};
+        binarySearch(arrTest,5);
+
+
+
+    }
+
+
+    /***
+     * 二分查找
+     * @param srcArray
+     * @param des
+     * @return
+     */
+    public static int binarySearch(Integer[] srcArray, int des) {
+        int low = 0;
+        int high = srcArray.length - 1;
+
+        while ((low <= high) && (low <= srcArray.length - 1)
+                && (high <= srcArray.length - 1)) {
+            int middle = (high + low) >> 1;
+            if (des == srcArray[middle]) {
+                return middle;
+            } else if (des < srcArray[middle]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+}
